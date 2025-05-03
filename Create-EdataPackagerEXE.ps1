e0eb29823c34ec3d7ff2525183efc6ff09b06a4e
TakaakiU <41781466+TakaakiU@users.noreply.github.com>
2025-05-03 02:39:11 +0000

    init

---
+++ Create-EdataPackagerEXE.ps1
@@ -0,0 +1,9 @@
+﻿# pyinstaller -n PyTkinterToPSScript --add-data "classes/config/settings.xml;classes/config" --add-data "classes/image/logo.png;classes/image" --hidden-import babel.numbers --clean --onefile --noconsole --collect-data tkinterdnd2 --paths="C:\Users\Administrator\Documents\Git\python\PyTkinterToPSScript" main.py
+pyinstaller -n PyTkinterToPSScript --add-data "classes/image/logo.png;classes/image" --hidden-import babel.numbers --clean --onefile --noconsole --collect-data tkinterdnd2 --paths="C:\Users\Administrator\Documents\Git\python\PyTkinterToPSScript" main.py
+# Update after release
+# pyinstaller .\PyTkinterToPSScript.spec
+
+# Copy the created executable file
+$copyFrom = ".\dist\PyTkinterToPSScript.exe"
+$copyTo = ".\classes\exe\PyTkinterToPSScript.exe"
+Copy-Item -Path $copyFrom -Destination $copyTo -Force
